d64345325d13d11c4aa093df9a8fa13795a98e47
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-08-22 00:02:03 +0300

    update city_model

--- models/city.py
+++ models/city.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, INTEGER, String, DateTime, ForeignKey
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db": 
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        name = ""
+        state_id = ""
