cd21db2afe4d989c3e6a17b678660255f3ae8170
shaco-star <sherifm846@gmail.com>
2023-09-21 04:05:51 -0700

    commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Run flask on port 5000"""
+from flask import Flask
+
+app = Flask(__name__)
+"""Flask application"""
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Home page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """hbnb page"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''C  page.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
